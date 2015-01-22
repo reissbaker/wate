66b2a312be14d5c85acab4d0f8303ea6a7d32f3e
Matt Baker <matthew.reiss.baker@gmail.com>
2015-01-21 23:35:44 -0800

    Support done() chains

--- build/lib/future.js
+++ build/lib/future.js
@@ -3,6 +3,7 @@ var Future = (function () {
     function Future(deferred) {
         this.done = function (cb) {
             deferred.done(cb);
+            return this;
         };
     }
     return Future;
--- lib/future.ts
+++ lib/future.ts
@@ -4,11 +4,12 @@ import Callback = require('./callback');
 import Deferred = require('./deferred');
 
 class Future<E, V> {
-  done: (cb: Callback<E, V>) => void;
+  done: (cb: Callback<E, V>) => Future<E, V>;
 
   constructor(deferred: Deferred<E, V>) {
     this.done = function(cb: Callback<E, V>) {
       deferred.done(cb);
+      return this;
     };
   }
 }
